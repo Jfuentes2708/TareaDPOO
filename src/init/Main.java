509dee0790a6ab91ae5a7123a18bc4a2a12a2958
Jfuentes2708 <javierfuentes270804@gmail.com>
2024-06-27 20:59:12 -0400

    subiendoproyecto

---
+++ src/init/Main.java
@@ -0,0 +1,23 @@
+package init;
+
+import java.awt.EventQueue;
+
+import ui.Interfaz;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfaz frame = new Interfaz();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+
+}
