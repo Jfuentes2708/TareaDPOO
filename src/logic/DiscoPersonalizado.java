ff7d206bce6e445664240556de5f21a2074cbc09
Jfuentes2708 <javierfuentes270804@gmail.com>
2024-08-08 21:26:05 -0400

    funcion disco personalizado

--- src/logic/DiscoPersonalizado.java
+++ src/logic/DiscoPersonalizado.java
@@ -4,6 +4,7 @@ public class DiscoPersonalizado extends Disco {
 
 	private float precioBase;
 	private float tamano;
+	private float precio;
 	private int cantidadDatosGrabados;
 
 
@@ -22,6 +23,9 @@ public class DiscoPersonalizado extends Disco {
 	public float getTamano() {
 		return tamano;
 	}
+	public float getPrecio() {
+		return precio;
+	}
 	public void setTamano(float tamano) {
 		this.tamano = tamano;
 	}
@@ -32,8 +36,30 @@ public class DiscoPersonalizado extends Disco {
 		this.cantidadDatosGrabados = cantidadDatosGrabados;
 	}
 
+	public float calacularPrecio(){
+		precio = 0;
+		if(this.getTipoSoporte().equals("CD")){
+			precioBase = 10;
+		}
+		if(this.getTipoSoporte().equals("DVD")){
+			precioBase=20;
+		}
+		if(0<cantidadDatosGrabados && cantidadDatosGrabados<=10){
+			precio = precioBase + 10;
+		}
+		else if(10<cantidadDatosGrabados && cantidadDatosGrabados<=15){
+			precio = precioBase + 20;
 
- 
+		}else if(15<cantidadDatosGrabados && cantidadDatosGrabados<=20){
+			precio = precioBase + 30;
+		}else{
+			precio = precioBase + 50;
+		}
+		return this.getPrecio();
+	}
+
+
+
 
 
 
